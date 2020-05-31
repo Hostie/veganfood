e175dc97e83874d14e9f686cc13efe07adbed427
root <dylan.levraud@ynov.com>
2020-05-31 15:39:01 +0200

    engagements + fix resto

---
+++ src/Form/SignFormType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+
+class SignFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('SignUp', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-large btn-info mt-2 mb-2'
+                ]
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
+
+
