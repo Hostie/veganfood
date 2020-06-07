7482f7096abd3b03591e05377181402ca0dddfc9
root <dylan.levraud@ynov.com>
2020-06-07 17:27:24 +0200

    le push qui fait plaiz avec tout responsive et tout qui marche aux petits oignons

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -12,7 +12,7 @@ use App\Form\RestaurantFormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserInterface; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -65,13 +65,10 @@ class AdminController extends AbstractController
       
         $restaurant = $repository -> findAll();
 
-       
-
-
         return $this->render('admin/restaurants.html.twig', [
             'restaurants' => $restaurant,
             'total' => $total,
-            'commands' => $commandArray            
+                 
         ]);
     }   
 
